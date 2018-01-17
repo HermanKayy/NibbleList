b46a7c3badd296b80b03a010cb44f4e6e4904390
Herman Kwan <HermanK@hermans-macbook-pro.local>
2018-01-16 16:12:07 -0800

    Initial Commit

---
+++ NibbleList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NibbleList
+//
+//  Created by Herman Kwan on 1/16/18.
+//  Copyright © 2018 Herman Kwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
